5b1e969fc456751d521291ee68da38d0948452d4
ziatariq785-svg <ziatariq785@gmail.com>
2026-01-03 12:29:11 +0500

    some more js

---
+++ 15th class/script.js
@@ -0,0 +1,22 @@
+console.log("Hello, World! This is the 15th class and 3rd class of js.");
+console.log(typeof parseFloat("10.988"));
+const num = parseFloat("10.988");
+
+console.log(typeof num, num);
+console.log(typeof "10.988");
+
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(10 / 20);
+console.log(10 % 3);
+console.log(10 ** 3);
+
+console.log("10" + "20");
+
+
+let a = 10;
+a = a + 1;
+a++;
+a--;
+console.log(a, "a");
